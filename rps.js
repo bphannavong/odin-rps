90a1fabf8b9c489927998b73b6c0485d6f727984
Billy Phannavong <billy.phannavong@gmail.com>
2022-04-27 18:15:33 -0700

    Finish playRound

--- rps.js
+++ rps.js
@@ -1,20 +1,27 @@
+const choices = ['ROCK', 'PAPER', 'SCISSORS'];
+const playerSelection = prompt('Rock, Paper, or Scissors?',);
+const computerSelection = computerPlay();
+
 function computerPlay() {
-    let choice = ['Rock', 'Paper', 'Scissors'];
     let rand = Math.floor(Math.random()* 3); //random from 0-2
-    
-    return choice[rand]; //choose random index from choices
+    return choices[rand]; //choose random index from choices
 }
 
 function playRound(playerSelection, computerSelection) {
+    //change both selections to uppercase for case sensitivity
     playerSelection = playerSelection.toUpperCase();
     computerSelection = computerSelection.toUpperCase();
 
-    if (!playerSelection) return 'empty!';
+    //check if player choice is valid
+    if (!choices.includes(playerSelection)) {return 'invalid entry!'};
+
+    //if  choices are the same, tie
     if (playerSelection === computerSelection) {return 'Tie!'}
-    
+
+    //show victory/defeats dependent on player/comp choices
     switch (playerSelection) { 
         case 'ROCK':
-            switch (computerSelection){
+            switch (computerSelection) {
                 case 'PAPER': {
                     return 'You lose! Paper beats Rock!';
                 }
@@ -22,9 +29,27 @@ function playRound(playerSelection, computerSelection) {
                     return 'You win! Rock beats Scissors!';
                 }
             }
+        case 'PAPER':
+            switch (computerSelection) {
+                case 'SCISSORS': {
+                    return 'You lose! Scissors beats Paper!';
+                }
+                case 'ROCK': {
+                    return 'You win! Paper beats Rock!';
+                }
+            }
+        case 'SCISSORS':
+            switch (computerSelection) {
+                case 'ROCK': {
+                    return 'You lose! Rock beats Scissors!';
+                }
+                case 'PAPER': {
+                    return 'You win! Scissors beats Paper!';
+                }
+            }
 
     }
 }
 
-const playerSelection = prompt('Rock, Paper, or Scissors?',);
-const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
